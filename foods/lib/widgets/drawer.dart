6d2e94328f1ef5e0c22be34c12824ff91ae621b3
cr <cbalanta@playtechla.com>
2025-10-22 12:12:44 -0500

    cambios
    Se modifico la el drawer

--- foods/lib/widgets/drawer.dart
+++ foods/lib/widgets/drawer.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DrawerPage extends StatelessWidget {
   const DrawerPage({super.key});
@@ -7,9 +7,38 @@ class DrawerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
         children: [
-          
+
+          SizedBox(height: 10,),
+          CircleAvatar(
+            radius: 100,
+            backgroundColor: Colors.amber, // color de fondo
+            child: Text(
+              'f', // iniciales
+              style: GoogleFonts.leckerliOne(
+                  fontSize: 50, color: Color.fromRGBO(109, 109, 109, 1)),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Center(
+            child: Text('Foods',style: GoogleFonts.leckerliOne(
+                    fontSize: 50, color: Color.fromRGBO(109, 109, 109, 1)),),
+          ),
+
+      Spacer(),
+
+          // Aquí agregamos el texto al final
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                'Desarrollado por Tu Nombre',  // Aquí puedes poner tu nombre o el de tu empresa
+                style: GoogleFonts.leckerliOne(
+                    fontSize: 16, color: Color.fromRGBO(109, 109, 109, 1)),
+              ),
+            ),
+          )
         ],
       ),
     );
